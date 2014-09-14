12044c040484e94ea59684bc065500635f2d6f64
Kevin C <clmememe@yahoo.co.nz>
2014-09-14 16:17:44 +0800

    8 pen styles

--- pen.py
+++ pen.py
@@ -40,7 +40,7 @@ class Pen():
 		self.resetPoint()
 		
 	def randAngle(self):
-		return uniform(0, 2 * math.pi)
+		return uniform(0, 2.0 * math.pi)
 		
 	def finalAngle(self):
 		return self.angle
@@ -72,11 +72,11 @@ class Pen():
 
 	def changeDirection(self):
 		self.angle = self.randAngle()
-		self.dAngle = uniform(0, 0.005)
-		self.ddAngle = uniform(0, 0.0005)
+		self.dAngle = uniform(-0.005, 0.005)
+		self.ddAngle = uniform(-0.0005, 0.0005)
 		self.changeDirectionChance = 0.001
-		self.dampDAngle = 1
-		self.dampDDAngle = 1
+		self.dampDAngle = 1.0
+		self.dampDDAngle = 1.0
 		self.maxLength = uniform(200, 300)
 		
 		# print "Pen maxLength = %d" % self.maxLength
@@ -138,39 +138,143 @@ class Clef(Pen):
 	def changeDirection(self):
 		Pen.changeDirection(self)
 	
-		self.dAngle = uniform(0, 0.0005)
+		self.dAngle = uniform(-0.0005, 0.0005)
 		self.ddAngle = 0
 		self.maxLength = uniform(500, 1100)
-		print "Clef maxLength = %d" % self.maxLength
 		
 class Roboglyph(Pen):
 	def finalAngle(self):
 		mult = math.pi / 3
-		return round(self.angle / mult) * mult		
+		return round(self.angle / mult) * mult
+	
+class Giraffes(Pen):
+	def changeDirection(self):
+		Pen.changeDirection(self)
 		
+		self.angle = -1.0 * math.pi / 2.0
+		self.changeDirectionChance = 0.005
+		
+class Commarabic(Pen):
+	def step(self):
+		Pen.step(self)
+	
+		# Weight increases linearly with iteration count, up to a max of 1
+		self.weight = self.age / self.maxLength
+	
+	def changeDirection(self):
+		Pen.changeDirection(self)
+		
+		dAngle = uniform(-0.01, 0.01)
+		ddAngle = uniform(-0.005, 0.005)
+		changeDirectionChance = 0.5
+		dampDAngle = 0.1
+		dampDDAngle = 0.1
+		maxLength = uniform(200, 800)
+	
+class BassClef(Pen):
+	def __init__(self):
+		Pen.__init__(self)
+		
+		self.scale = 2.0
+		
+	def finalAngle(self):
+		return 2 * math.pi * math.sin(100 * self.angle)
+		
+	def step(self):
+		Pen.step(self)
+	
+		# Weight decreases linearly with iteration count, from 1.8 to 0
+		self.weight = 1.8 * (1.0 - self.age / self.maxLength)
+		
+	def changeDirection(self):
+		Pen.changeDirection(self)
+	
+		self.dAngle = uniform(-0.0005, 0.0005)
+		self.ddAngle = 0
+		self.maxLength = uniform(500, 1100)
+	
+class Staccato(Pen):
+	def __init__(self):
+		Pen.__init__(self)
+		
+		self.scale = 2.5
+		
+	def finalAngle(self):
+		return 2 * math.pi * math.sin(100 * self.angle)
+		
+	def step(self):
+		Pen.step(self)
+	
+		# Weight decreases linearly with iteration count, from 2 to 0
+		self.weight = 2.0 * (1.0 - self.age / self.maxLength)
+		# Weight = 0.5 for 100 steps, occurring every 200 steps
+		if (self.age % 200) < 100:
+			self.weight = 0.5
+			
+	def changeDirection(self):
+		Pen.changeDirection(self)
+	
+		self.dAngle = uniform(-0.0005, 0.0005)
+		self.ddAngle = 0
+		self.maxLength = uniform(500, 1100)
+	
+class Madman(Pen):
+	def finalAngle(self):
+		if self.age % 70 < 60:
+			return math.pi * 2 * math.cos(self.angle)
+		else:
+			return uniform(-1.0 * math.pi, math.pi)
+			
+	def changeDirection(self):
+		Pen.changeDirection(self)
+	
+		self.weight = uniform(0, 1)
+		self.ddAngle = uniform(-0.0001, 0.0001)
+		self.changeDirectionChance = 0.1
+		self.dampDAngle = 0.9
+		self.dampDDAngle = 0.9
+		self.maxLength = uniform(500, 1100)
+
 print ("""
-	[1] Squiggle
-	[2] Clef
-	[3] Roboglyph
+	[1] Clef
+	[2] Roboglyph
+	[3] Loopy
 	[4] Giraffes
-	[5] Leonardo
-	[6] Fantasy
+	[5] Commarabic
+	[6] Bass Clef
+	[7] Staccato
+	[8] Madman
 	""")
 
 styleNum = input("Type the number of the style you want to use: ")
 validChoice = 1
 
 if styleNum == 1:
-	penStyle = Pen()
-	styleName = "Squiggle "
-elif styleNum == 2:
 	penStyle = Clef()
 	styleName = "Clef "
-elif styleNum == 3:
+elif styleNum == 2:
 	penStyle = Roboglyph()
 	styleName = "Roboglyph "
+elif styleNum == 3:
+	penStyle = Pen()
+	styleName = "Loopy "
+elif styleNum == 4:
+	penStyle = Giraffes()
+	styleName = "Giraffes "
+elif styleNum == 5:
+	penStyle = Commarabic()
+	styleName = "Commarabic "
+elif styleNum == 6:
+	penStyle = BassClef()
+	styleName = "Bass Clef "
+elif styleNum == 7:
+	penStyle = Staccato()
+	styleName = "Staccato "
+elif styleNum == 8:
+	penStyle = Madman()
+	styleName = "Madman "
 else:
-	print ("Only the listed numbers are valid!")
+	print ("Only the listed numbers are valid, try again!")
 	validChoice = 0
 
 if validChoice == 1:
